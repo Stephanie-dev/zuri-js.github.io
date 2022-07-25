5ff3fa4eb4f2138de1a1cd2c2a35bb67c0501f7b
stephanie <stephaniechiamaka63@gmail.com>
2022-07-25 01:51:25 +0100

    first commit

---
+++ zuri.js
@@ -0,0 +1,8 @@
+let title = "Okafor Chiamaka Stephanie";
+alert(`My name is ${title}`);
+
+let height = "Six Feet Tall";
+alert(`I am ${height}`);
+
+let country = "Nigeria";
+alert(`I am from ${country}`);
