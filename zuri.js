babf8a6d5eca99900bbf2487cd967fda4f5e3146
Stephanie-dev <101674940+Stephanie-dev@users.noreply.github.com>
2022-07-27 21:24:35 +0100

    Update zuri.js

--- zuri.js
+++ zuri.js
@@ -1,4 +1,4 @@
 let title = "Okafor Chiamaka Stephanie.";
-let height = "Six Feet Tall..";
+let height = "Six Feet Tall.";
 let country = "Nigeria.";
 alert(`My name is ${title} \n I am ${height} \n  I am from ${country}`);
